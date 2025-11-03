98eca9d22678a2968b891df2f419147b27a83073
Thiha Thant Sin <42347119+ThihaThantSin777@users.noreply.github.com>
2025-11-03 22:49:35 +0700

    make encrypt method static

--- lib/utils/encrypt_utils.dart
+++ lib/utils/encrypt_utils.dart
@@ -1,20 +1,16 @@
 import 'package:encrypt/encrypt.dart';
 
 class EncryptUtils {
-  static String encryptText(String plainText) {
-    final key = Key.fromLength(32);
-    final iv = IV.fromLength(8);
-    final encrypter = Encrypter(Salsa20(key));
+  static final key = Key.fromLength(32);
+  static final iv = IV.fromLength(8);
+  static final encrypter = Encrypter(Salsa20(key));
 
+  static String encryptText(String plainText) {
     final encrypted = encrypter.encrypt(plainText, iv: iv);
     return encrypted.base64;
   }
 
   static String decryptText(String encryptedBase64) {
-    final key = Key.fromLength(32);
-    final iv = IV.fromLength(8);
-    final encrypter = Encrypter(Salsa20(key));
-
     final encrypted = Encrypted.fromBase64(encryptedBase64);
     final decrypted = encrypter.decrypt(encrypted, iv: iv);
     return decrypted;
